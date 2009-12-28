e150460d4c36a52d8df1ab5c59902de19fc0c19a
Standard <Standard@Standard-PC>
2009-12-28 20:40:42 +0100

    Includes nach Frontend und Backend aufgeteilt, um Speicher und Laufzeit zu sparen. https://bitbucket.org/webvariants/rexblog/changeset/c413c8d2922e/. Kleinere Patches an REXblog, um es mit unseren eigenen AddOns kompatibel zu halten. https://bitbucket.org/webvariants/rexblog/changeset/96119b29086f/

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
 
-/*
+/**
  * Copyright (c) 2009, mediastuttgart werbeagentur, http://www.mediastuttgart.de
  *
  * Diese Datei steht unter der MIT-Lizenz. Der Lizenztext befindet sich in der
@@ -9,7 +9,7 @@
  *
  * http://www.opensource.org/licenses/mit-license.php
  * http://de.wikipedia.org/wiki/MIT-Lizenz
-*/
+ */
 
 if(defined('_rex488_PATH')) return;
 
@@ -42,47 +42,82 @@ $REX['ADDON']['supportpage']['rexblog'] = 'http://bitbucket.org/mediastuttgart/r
 
 $REX['PERM'][] = "rexblog[]";
 
-// Funktionen einbinden
+// Autoload für Klassen
 
-require dirname(__FILE__) . '/functions/function.backend.core.php';
-require dirname(__FILE__) . '/functions/function.frontend.core.php';
-
-// Autoload für Klassen definieren
-
-function __autoload($classname)
+function _rex488_autoload($params)
 {
   static $classes = array(
-    '_rex488_BackendBase'               => 'backend/class.backend.base.php',
-    '_rex488_BackendCache'              => 'backend/class.backend.cache.php',
-    '_rex488_BackendException'          => 'backend/class.backend.exception.php',
-    '_rex488_BackendCategoryInterface'  => 'backend/interface/interface.backend.categories.php',
-    '_rex488_BackendPost'               => 'backend/class.backend.post.php',
-    '_rex488_BackendCategories'         => 'backend/class.backend.categories.php',
-    '_rex488_FrontendBase'              => 'frontend/class.frontend.base.php',
-    '_rex488_FrontendDesignator'        => 'frontend/class.frontend.designator.php',
-    '_rex488_FrontendCategories'        => 'frontend/class.frontend.categories.php',
-    '_rex488_FrontendCategory'          => 'frontend/class.frontend.category.php',
-    '_rex488_FrontendMetadata'          => 'frontend/class.frontend.metadata.php',
-    '_rex488_FrontendMetadataCategory'  => 'frontend/class.frontend.metadata.category.php',
-    '_rex488_FrontendMetadataPost'      => 'frontend/class.frontend.metadata.post.php',
-    '_rex488_FrontendPost'              => 'frontend/class.frontend.post.php',
-    '_rex488_FrontendPagination'        => 'frontend/class.frontend.pagination.php',
+  '_rex488_BackendBase'               => 'backend/class.backend.base.php',
+  '_rex488_BackendCache'              => 'backend/class.backend.cache.php',
+  '_rex488_BackendException'          => 'backend/class.backend.exception.php',
+  '_rex488_BackendCategoryInterface'  => 'backend/interface/interface.backend.categories.php',
+  '_rex488_BackendPost'               => 'backend/class.backend.post.php',
+  '_rex488_BackendCategories'         => 'backend/class.backend.categories.php',
+  '_rex488_FrontendBase'              => 'frontend/class.frontend.base.php',
+  '_rex488_FrontendDesignator'        => 'frontend/class.frontend.designator.php',
+  '_rex488_FrontendCategories'        => 'frontend/class.frontend.categories.php',
+  '_rex488_FrontendCategory'          => 'frontend/class.frontend.category.php',
+  '_rex488_FrontendMetadata'          => 'frontend/class.frontend.metadata.php',
+  '_rex488_FrontendMetadataCategory'  => 'frontend/class.frontend.metadata.category.php',
+  '_rex488_FrontendMetadataPost'      => 'frontend/class.frontend.metadata.post.php',
+  '_rex488_FrontendPost'              => 'frontend/class.frontend.post.php',
+  '_rex488_FrontendPagination'        => 'frontend/class.frontend.pagination.php',
   );
+
+  $classname = $params['subject'];
 
   if(isset($classes[$classname]))
   {
+    // Angeforderte Klassen einbinden
+
     require_once dirname(__FILE__) . '/classes/' . $classes[$classname];
+
+    // Liefert true zurück nachdem die Klasse erfolgreich eingebunden wurde
+
+    return true;
   }
+
+  return false;
 }
 
-// Subpages definieren
+// Prüft ob bereits eine autoload Funktion definiert wurde
 
-$REX['ADDON']['rexblog']['SUBPAGES'] = array(
-        array('categories', 'Kategorien', '', array('clang' => $REX['CUR_CLANG']))
-);
+if(!function_exists('__autoload'))
+{
+  function __autoload($classname)
+  {
+    if(_rex488_autoload(array('subject' => $classname)) === false)
+    {
+      rex_register_extension_point('__AUTOLOAD', $classname);
+    }
+  }
+}
+  else
+{
+  rex_register_extension('__AUTOLOAD', '_rex488_autoload');
+}
 
-// Stylesheets und Javascript einbinden
+// Backend und Frontend Eigenschaften, Funktionen und Assets
 
-rex_register_extension('PAGE_HEADER', '_rex488_add_pageheader');
+if($REX['REDAXO'])
+{
+  // Subpages definieren
 
-?>
+  $REX['ADDON']['rexblog']['SUBPAGES'] = array(
+          array('categories', 'Kategorien', '', array('clang' => $REX['CUR_CLANG']))
+  );
+
+  // Stylesheets und Javascript einbinden
+
+  require dirname(__FILE__) . '/functions/function.backend.core.php';
+
+  // Backend Funktionen inkludieren
+
+  rex_register_extension('PAGE_HEADER', '_rex488_add_pageheader');
+}
+  else
+{
+  // Frontend Funktionen inkludieren
+
+  require dirname(__FILE__) . '/functions/function.frontend.core.php';
+}
